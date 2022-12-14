42b7885269f15e2d292ea219f8d6c6fcccf6d311
Stephan Griesel <sgriesel@gmail.com>
2022-12-14 06:50:25 +0100

    refactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({imgUrl, title, paragraphText, buttonText}) => {
     return (
         <div className='flex flex-col p-4 m-4 rounded-lg bg-slate-200 shadow-xl transition-all duration-1000 hover:shadow-none cursor-pointer hover:scale-95'>
             <div>
-                <img src="https://doodleipsum.com/700x525/hand-drawn?i=a96b513e1e4deb10b96d2d852ebd824e" alt="illustration" />
-                <h2 className='text-center font-bold'>Lorem ipsum dolor</h2>
-                <div className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis numquam totam dignissimos ipsum dolore doloribus assumenda repellendus molestiae vel? Atque quam optio odio officia expedita dicta, praesentium iste quia perferendis?</div>
+                <img src={imgUrl} alt="illustration" />
+                <h2 className='text-center font-bold'>{title}</h2>
+                <div className='text-center'>{paragraphText}</div>
             </div>
             <div className='flex justify-center'>
-                <button className='bg-white'><a href="#" className='text-center m-2'>See more</a></button>
+                <button className='bg-white'><a href="#" className='text-center m-2'>{buttonText}</a></button>
             </div>
         </div>
     )
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -4,10 +4,10 @@ import Card from './Card';
 const CardContainer = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-4'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card imgUrl={"https://doodleipsum.com/700x525/hand-drawn?i=222b26c23a8f805f506d35b0a536569c"} title="Title" paragraphText="Paragraph Text" buttonText="Click Me" />
+        <Card imgUrl={"https://doodleipsum.com/700x525/hand-drawn?i=1176f4ffe82e1314ef9a71b9599bc1fa"} title="Title" paragraphText="Paragraph Text" buttonText="Click Me" />
+        <Card imgUrl={"https://doodleipsum.com/700x525/hand-drawn?i=1e5005d8273d015ff16363c9ec16e7b1"} title="Title" paragraphText="Paragraph Text" buttonText="Click Me" />
+        <Card imgUrl={"https://doodleipsum.com/700x525/hand-drawn?i=dee23ea78d4bed889e6b2c07f25f3a13"} title="Title" paragraphText="Paragraph Text" buttonText="Click Me" />
     </div>
   )
 }
