1c44005734e2e0a59ef5bd7154283243506e8237
Stephan Griesel <sgriesel@gmail.com>
2022-12-14 07:17:35 +0100

    test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import React, {useEffect} from 'react';
 import CardContainer from './components/CardContainer';
 
 function App() {
+
+  useEffect(() => {
+    async function fetchDataFromAPIEndpoint(){
+      const cards = await fetch('/api/fetchNotion')
+      .then((res) => res.json()
+      .then((data) => console.log("check data results",data.results)))
+    }
+    fetchDataFromAPIEndpoint()
+  })
+
 
   return (
     <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
