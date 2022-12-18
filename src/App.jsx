4225befc459e00816e7fbc8b82d3f4e80581f9b2
Stephan Griesel <sgriesel@gmail.com>
2022-12-18 10:01:01 +0100

    timeout add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import CardContainer from './components/CardContainer';
 
-function App() {
-  let [notionData, setNotionData] = useState(null);
-
-  const fetchNotionData = async () => {
-    const data = await fetch('/api/fetchNotion');
-    const allNotionData = await data.json();
-    console.log("notion data in app", allNotionData);
-    setNotionData(allNotionData);
-  }
+const App = () => {
+  const [notionData, setNotionData] = useState(null);
 
   useEffect(() => {
-    fetchNotionData();
+    const fetchNotionData = async () => {
+      const data = await fetch('/api/fetchNotion');
+      const allNotionData = await data.json();
+      console.log("notion data in app", allNotionData);
+      setNotionData(allNotionData);
+    }
+    const timer = setTimeout(() => {
+      fetchNotionData();
+    }, 1);
+
+    return () => clearTimeout(timer);
   },[])
+
+  if (!notionData){
+    return <div className='App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen'></div>
+  }
 
 
   return (
