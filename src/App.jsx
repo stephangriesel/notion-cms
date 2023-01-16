5c8c47dbca493ecf34e2779e27ff22577cbe4ff0
Stephan Griesel <sgriesel@gmail.com>
2023-01-16 16:17:23 +0100

    update data show condition & add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import CardContainer from './components/CardContainer';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 const App = () => {
   const [notionData, setNotionData] = useState(null);
@@ -19,13 +21,18 @@ const App = () => {
   },[])
 
   if (!notionData){
-    return <div className='App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen'></div>
+    return <div className='App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen text-center'>
+      <p>You meant to run that locally? 🤔 </p>
+      <p>If you running Netlify functions run: netlify dev</p>
+      </div>
   }
 
 
   return (
-    <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
+    <div className="App bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-full">
+      <NavBar />
       <CardContainer notionData={notionData}/>
+      <Footer />
     </div>
   )
 }
