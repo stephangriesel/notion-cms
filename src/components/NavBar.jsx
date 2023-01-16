fb57cee79858e408ce2cfd843c964630efc73226
Stephan Griesel <sgriesel@gmail.com>
2023-01-16 08:39:33 +0100

    navbar wip

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex justify-center'>
+        <div>
+            Logo
+        </div>
+        <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
